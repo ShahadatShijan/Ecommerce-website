01144117f747b5b2c2a1a2ea78bfd608b5276fdb
Shahadat Hossain <shahadatshijan@gmail.com>
2023-09-26 13:38:02 +0600

    added customize colors, font-family and config

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    screens:{
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px'
+    },
+    container:{
+      center: true,
+      padding: '1rem'
+    },
+    extend: {
+      fontFamily: {
+        pappins: "'Poppins', sans-serif",
+        roboto: "'Roboto', sans-serif"
+      },
+      colors:{
+        'primary':'#FD3D57'
+      }
+    },
+  },
+  plugins: [require('@tailwindcss/forms')], 
+}
+
